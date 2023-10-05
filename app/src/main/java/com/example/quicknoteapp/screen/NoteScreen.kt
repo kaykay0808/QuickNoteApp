2fe99e60fef0ce789d92d37d0cced91e87d49f94
kay <kayshiro@live.no>
2023-10-05 09:25:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/quicknoteapp/screen/NoteScreen.kt
@@ -0,0 +1,15 @@
+package com.example.quicknoteapp.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NoteScreen() {
+
+}
+
+@Preview
+@Composable
+fun NoteScreenPreview() {
+
+}
