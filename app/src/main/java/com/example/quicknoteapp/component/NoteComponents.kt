a345ba408ebfa05407b57acaac5533c35cbe6235
kay <kayshiro@live.no>
2023-10-08 21:37:29 +0200

    Creating TextField and Button

---
+++ app/src/main/java/com/example/quicknoteapp/component/NoteComponents.kt
@@ -0,0 +1,89 @@
+package com.example.quicknoteapp.component
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun NoteInputText(
+    modifier: Modifier = Modifier,
+    text: String,
+    label: String,
+    maxLine: Int = 1,
+    onTextChange: (String) -> Unit,
+    onImeAction: () -> Unit = {}
+) {
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    TextField(
+        modifier = modifier,
+        value = text,
+        onValueChange = onTextChange,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.Transparent
+        ),
+        maxLines = maxLine,
+        label = { Text(text = label) },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                onImeAction()
+                keyboardController?.hide()
+            }
+        )
+    )
+}
+
+@Composable
+fun NoteButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true
+) {
+    //okoko
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        shape = CircleShape,
+        enabled = enabled
+    ){
+        Text(text = text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteInputTextPreview() {
+    NoteInputText(
+        label = "Label",
+        text = "some random text",
+        onTextChange = {},
+    )
+}
+
+@Preview
+@Composable
+fun NoteButtonPreview() {
+    NoteButton(
+        onClick = {},
+        text = "BUTTON"
+
+    )
+}
